8bc582e95f2203de33063bcdf7e58d456bc62933
cwj9659-droid <cwj9659@gmail.com>
2026-02-27 14:49:07 +0900

    123

--- End_point_MainPage/src/main/java/com/cx/web/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cx.web.repository;
-
-import com.cx.web.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, String> {
-    // 기본 CRUD 제공
-}
--- traffic_on_backend_suyeon/src/main/java/com/cx/web/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cx.web.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cx.web.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, String> {
-
-}
