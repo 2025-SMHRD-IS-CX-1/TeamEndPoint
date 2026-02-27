3cb28b8be904c70f5ee7ecd15e59b75c2b4b3470
cwj9659-droid <cwj9659@gmail.com>
2026-02-27 14:49:07 +0900

    123

--- End_point_MainPage/src/main/java/com/cx/web/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cx.web.repository;
-
-import com.cx.web.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-    Member findByMemId(String memId);
-}
